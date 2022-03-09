b1222cf9f9d17bab33fb750d110c6514e5c6854f
Morozkin_Ilya <morozkin.ilya86@mail.ru>
2022-03-09 21:03:14 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+void Numbers(int m, int n)
+{
+    if (m < n)
+    {
+        FindSum(m, n);
+    }
+
+}
+void FindSum(int m, int n)
+{
+    int sum = m;
+    while (m < n)
+    {
+        m++;
+        sum = sum + m;
+    }
+    Console.Write(sum);
+}
+
+Numbers(1, 15);
+Console.WriteLine();
+Numbers(4, 8);
