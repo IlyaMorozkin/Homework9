467db295e9f5ddcf96c94778c549cd3f741f8cc8
Morozkin_Ilya <morozkin.ilya86@mail.ru>
2022-03-09 21:00:40 +0300

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void Numbers(int m, int n)
+{
+    if (m < n)
+    {
+        Console.Write($"{m}");
+        Fill(m,n);
+    }
+
+}
+void Fill(int m,int n)
+{
+    while(m < n)
+    {
+        m++;
+        Console.Write($", {m} ");
+    }
+
+}
+
+Numbers(1, 5);
+Console.WriteLine();
+Numbers(4, 8);
